b3d083fac8853fb71681ed57bc405d08adfe9795
baisebaoma <baisebaoma@foxmail.com>
2023-06-20 01:18:38 +0800

    Added Saver and Switcher

---
+++ GenshinAccountSaver.bat
@@ -0,0 +1,16 @@
+@echo off
+title 《原神》账号保存工具 by baisebaoma
+echo 本工具将快照当前的账号登录状态。
+set /p a=输入当前《原神》已经登录的账号的一个昵称：
+if "%usrname%" == "" set usrname=default
+reg export "HKEY_CURRENT_USER\Software\miHoYo\原神" "%usrname%.reg"
+if %ERRORLEVEL% == 0 (
+    echo 成功。在您下次需要登录这个账号的时候，请使用 GenshinAccountSwitcher.bat。
+) else (
+    echo 失败了！
+)
+
+:goodbye
+echo 按任意键退出
+pause>nul
+exit
